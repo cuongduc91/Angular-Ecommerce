eb0b790a9e58321b9e6601479c8127a8969d5a36
Cuong <bdcuong91@gmail.com>
2020-07-16 10:01:12 +0200

    add products router with category

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -78,9 +78,49 @@ router.get('/:prodId', (req, res) => {
         })
       } else {
         res.json({
-          message: 'No product found'
+          message: `No product found with id: ${productId}`
         });
       }
     }).catch(err => console.log(err));
+})
+
+/* GET products with category */
+router.get('/category/:catName', (req, res) => {
+  let page = (req.query.page !== undefined && req.query.page !== 0) ? req.query.page : 1;
+  const limit = (req.query.limit !== undefined && req.query.limit !== 0) ? req.query.limit : 10;
+  let startValue;
+  let endValue;
+  if (page > 0) {
+    startValue = (page * limit) - limit; // 0, 10, 20, 30
+    endValue = page * limit; // 10, 20, 30, 40
+  } else {
+    startValue = 0;
+    endValue = 10;
+  }
+  database.table('db.products as p').join([{
+    table: "db.categories as c",
+    on: `c.id= p.cat_id where c.title like '${req.params.catName}'`
+  }]).withFields([
+    'c.title as category',
+    'p.title as name',
+    'p.price',
+    'p.quantity',
+    'p.description',
+    'p.image',
+    'p.id'
+  ]).slice(startValue, endValue).sort({
+    id: .1
+  }).getAll().then(prods => {
+    if (prods.length > 0) {
+      res.status(200).json({
+        count: prods.length,
+        products: prods
+      })
+    } else {
+      res.json({
+        message: 'No product found with category ${req.params.catName}'
+      });
+    }
+  }).catch(err => console.log(err));
 })
 module.exports = router;
