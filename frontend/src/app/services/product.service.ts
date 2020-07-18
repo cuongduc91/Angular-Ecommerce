11a882055ba934fd65d228151517a0b2dae2ac70
Cuong <bdcuong91@gmail.com>
2020-07-18 23:02:10 +0200

    getSingleProduct, create product and cart models

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { ServerResponse } from 'http';
+import { ProductModelServer } from '../models/product/product.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,11 +13,16 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   /* get all products */
-  getAllProducts(limitOfResults = 10) {
-    return this.http.get(this.url + 'products', {
+  getAllProducts(limitOfResults = 10): Observable<ServerResponse> {
+    return this.http.get<ServerResponse>(this.url + 'products', {
       params: {
         limit: limitOfResults.toString(),
       },
     });
+  }
+
+  // GET single product from server
+  getSingleProduct(id: number): Observable<ProductModelServer> {
+    return this.http.get<ProductModelServer>(this.url + '/products' + id);
   }
 }
