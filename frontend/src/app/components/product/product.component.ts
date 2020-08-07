801a0232a683feeac9c9d9813ab05f0aba460cbe
Cuong <bdcuong91@gmail.com>
2020-08-07 16:24:05 +0200

    product page completed

--- frontend/src/app/components/product/product.component.ts
+++ frontend/src/app/components/product/product.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { ProductService } from 'src/app/services/product.service';
-import { CartService } from 'src/app/services/cart.service';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { CartService } from '../../services/cart.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { map } from 'rxjs/operators';
+
 declare let $: any;
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -13,29 +15,34 @@ export class ProductComponent implements OnInit, AfterViewInit {
   id: number;
   product;
   thumbImages: any[] = [];
+
+  @ViewChild('quantity') quantityInput;
+
   constructor(
     private productService: ProductService,
     private cartService: CartService,
     private route: ActivatedRoute
-  ) {
+  ) {}
+
+  ngOnInit(): void {
     this.route.paramMap
       .pipe(
         map((param: ParamMap) => {
-          return parseInt(param.get('id'));
+          // @ts-ignore
+          return param.params.id;
         })
       )
       .subscribe((prodId) => {
         this.id = prodId;
         this.productService.getSingleProduct(this.id).subscribe((prod) => {
           this.product = prod;
+
           if (prod.images !== null) {
-            this.thumbImages = prod.images.split(':');
+            this.thumbImages = prod.images.split(';');
           }
         });
       });
   }
-
-  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     // Product Main img Slick
@@ -76,5 +83,44 @@ export class ProductComponent implements OnInit, AfterViewInit {
     if (zoomMainProduct) {
       $('#product-main-img .product-preview').zoom();
     }
+  }
+
+  Increase() {
+    let value = parseInt(this.quantityInput.nativeElement.value);
+
+    if (this.product.quantity >= 1) {
+      value++;
+
+      if (value > this.product.quantity) {
+        value = this.product.quantity;
+      }
+    } else {
+      return;
+    }
+
+    this.quantityInput.nativeElement.value = value.toString();
+  }
+
+  Decrease() {
+    let value = parseInt(this.quantityInput.nativeElement.value);
+
+    if (this.product.quantity > 0) {
+      value--;
+
+      if (value <= 1) {
+        value = 1;
+      }
+    } else {
+      return;
+    }
+
+    this.quantityInput.nativeElement.value = value.toString();
+  }
+
+  addToCart(id: number) {
+    this.cartService.AddProductToCart(
+      id,
+      this.quantityInput.nativeElement.value
+    );
   }
 }
